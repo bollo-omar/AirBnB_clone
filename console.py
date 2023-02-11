4d1dec11e30b811814db3be272b86b2f89723812
bollo-omar <bolloaggrey@gmail.com>
2023-02-11 07:02:05 -0800

    initial commit

---
+++ console.py
@@ -0,0 +1,8 @@
+#!/bin/env bash
+# Write beautiful code that passes the pycodestyle checks.
+
+def rectangle(length, width):
+    return length * width;
+
+if __name__ ='__main__':
+    print(rectangle(4,8))
